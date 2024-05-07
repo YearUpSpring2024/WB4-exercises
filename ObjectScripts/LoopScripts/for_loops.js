f4b6659bda8335a2d125d4586a68b9731736ecca
Etson <etsond@ufl.edu>
2024-05-07 11:31:31 -0400

    loops exercises

---
+++ ObjectScripts/LoopScripts/for_loops.js
@@ -0,0 +1,8 @@
+"use strict";
+
+
+let message = "I love loops";
+
+for (let i = 0; i < 7; i++) {
+    console.log(message)
+}
---
+++ ObjectScripts/LoopScripts/while_loops.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let message = "I love while loops";
+let i = 0;
+
+while (i < 7) {
+    console.log(message);
+    i++;
+}
